a168bab725cf4074fee03b533acc5e2e61ed0528
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-07-10 16:33:57 +0600

    templates

--- chai/views.py
+++ chai/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def all_chai(request):
     chais = ChaiVarity.objects.all()
     return render(request,'all_chai.html',{'chais': chais})
 
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, id=id)
+    return render(request, 'chai_detail.html', {'chai': chai})
