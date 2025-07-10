9ad9fb0b2802753d835e9acd01fec0e2b2c85fbe
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-07-10 13:23:48 +0600

    config

--- chairaurDjango/settings.py
+++ chairaurDjango/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = ['127.0.0.1']
 
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
