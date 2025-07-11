ab65e66c7ea64821dcc1f7e696e6e1e4c642d49f
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-07-11 17:59:35 +0600

    config done

---
+++ chai/forms.py
@@ -0,0 +1,7 @@
+from django import from .forms 
+from .models import ChaiVarity
+
+
+class ChaiVarityForm(forms.Form):
+    chai_varity = forms.ModelChoiceField(queryset=ChaiVarity.objects.all(), label='Select Chai Varity')
+    
--- chai/views.py
+++ chai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import ChaiVarity
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarityForm
 
 # Create your views here.
 def all_chai(request):
@@ -10,3 +11,16 @@ def all_chai(request):
 def chai_detail(request, chai_id):
     chai = get_object_or_404(ChaiVarity, id=id)
     return render(request, 'chai_detail.html', {'chai': chai})
+
+def chai_stores_view(request):
+    store = None
+    if request.method == 'POST':
+    form = ChaiVarityForm(request.POST)
+    if form.is_valid():
+       chai_varity =  form.cleaned_data['chai_varity']
+       stores =  Store.objects.filter(chai_varieties= chai_varity)
+
+    else:
+        form = ChaiVarityForm()
+    return render(request,'chai_stores.html',
+    {'stores' : stores,'form': form})
