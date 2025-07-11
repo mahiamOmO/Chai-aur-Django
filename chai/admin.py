4174f4733bc3f350fcb8e6d161fb621cec91a215
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-07-11 13:32:24 +0600

    models.py updated

--- chai/admin.py
+++ chai/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, Store, ChaiCertificate
 
-# Register your models here.
-admin.site.register(ChaiVarity)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'Type', 'date_added')  
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',) 
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+admin.site.register(ChaiVarity, ChaiVarityAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
--- chai/models.py
+++ chai/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
 class ChaiVarity(models.Model):
     CHAI_TYPE_CHOICE = [
-        ('ML','MASALA'),
-        ('AL','MASALA'),
-        ('BL','KIWI'),
-        ('XL','PLAIN'),
-        ('XL','GINGER'),
+        ('ML', 'MASALA'),
+        ('AL', 'ASSAM'),
+        ('BL', 'KIWI'),
+        ('XL', 'PLAIN'),
+        ('XG', 'GINGER'),
     ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='chais/')
@@ -16,23 +16,19 @@ class ChaiVarity(models.Model):
     Type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
     description = models.TextField(default='')
 
-
     def __str__(self):
-        return self.name 
-    
+        return self.name
 
-    # One to Many
 class ChaiReview(models.Model):
-    chai =  models.ForeignKey(ChaiVarity,on on_delete= models.CASACDE,related_name='reviews')
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chai_reviews')
     rating = models.IntegerField()
     comment = models.TextField()
     date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f'{self.user.username}' review for {self.chai.name}'
+        return f"{self.user.username} review for {self.chai.name}"
 
-# Many to many
 class Store(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -41,14 +37,11 @@ class Store(models.Model):
     def __str__(self):
         return self.name
 
-# One to one
-
 class ChaiCertificate(models.Model):
-    chai = models.OneToOneFiels(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
-    on_delete = models.CASACADE,related_name='certificate')
-    certificate_number = models.CharField(max_length= 100)
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
     issued_date = models.DateTimeField(default=timezone.now)
-    valid_untill = models.DateTimeField()
+    valid_until = models.DateTimeField()
 
     def __str__(self):
-        return f'Certificate for {self.chai.name} - {self.certificate_number}'
+        return f"Certificate for {self.chai.name} - {self.certificate_number}"
